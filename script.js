05d56ff2c73775918bdf2312561ad0408b836568
Julianne Venancio <venanciojulianne@gmail.com>
2024-11-01 11:58:16 -0300

    edição estrutura html e css

---
+++ script.js
@@ -0,0 +1,6 @@
+const infos = {
+nome: document.getElementById("nomeCliente"),
+data: document.getElementById("dataCliente"),
+email: document.getElementById("emailCliente"),
+numero: document.getElementById("numCliente")
+}
