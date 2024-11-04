bff9622ee54d4eb061d460ad3ec394dff957096a
Julianne Venancio <venanciojulianne@gmail.com>
2024-11-04 12:01:20 -0300

    conserto mostrar infos e add botão delete

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ nome: document.getElementById("nomeCliente"),
 data: document.getElementById("dataCliente"),
 email: document.getElementById("emailCliente"),
 numero: document.getElementById("numCliente")
-}
-const botao= document.getElementById("botaoSub")
+};
+const botao= document.getElementById("btn1")
 const spans= document.querySelectorAll(".span")
+const btnDelete= document.getElementById("btn2") 
 
 class NovaInfo {
     constructor (nomeCliente, dataCliente, emailCliente,numCliente){
@@ -14,15 +15,18 @@ class NovaInfo {
         this.emailCliente= emailCliente;
         this.numCliente= numCliente;
     }
-}
-
-function mostrar(){
+mostrar(){
     spans[0].innerText= this.nomeCliente;
     spans[1].innerText= this.dataCliente;
     spans[2].innerText= this.emailCliente;
     spans[3].innerText= this.numCliente;
 }
 
+exluir(){
+    spans.forEach(span=> span.innerText="")
+}
+}
+// eventos para adicionar a informação e deletá-las
 botao.addEventListener("click", () => {
     const novaInfo = new NovaInfo(
       infos.nome.value,
@@ -31,4 +35,9 @@ botao.addEventListener("click", () => {
       infos.numero.value
     );
     novaInfo.mostrar();
+});
+
+btnDelete.addEventListener("click",()=>{
+    const novaInfo=new NovaInfo();
+    novaInfo.exluir();
 });
